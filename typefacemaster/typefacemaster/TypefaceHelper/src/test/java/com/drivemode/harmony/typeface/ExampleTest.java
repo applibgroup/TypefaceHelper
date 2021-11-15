dbee4d15714690e172b61852a1c4bbdb003b5842
YashDaga17 <dagayash17@gmail.com>
2021-11-15 16:08:53 +0530

    Adding TypefaceMaster of HMOS

---
+++ typefacemaster/typefacemaster/TypefaceHelper/src/test/java/com/drivemode/harmony/typeface/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.drivemode.harmony.typeface;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
---
+++ typefacemaster/typefacemaster/entry/src/test/java/com/example/typefacemaster/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.example.typefacemaster;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
