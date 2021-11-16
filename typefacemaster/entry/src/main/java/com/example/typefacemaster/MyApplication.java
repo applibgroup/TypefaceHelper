24ce6c8c5bff5df8f0b73086d88e6e9956446728
YashDaga17 <dagayash17@gmail.com>
2021-11-16 09:50:35 +0530

    Adding files

---
+++ typefacemaster/entry/src/main/java/com/example/typefacemaster/MyApplication.java
@@ -0,0 +1,16 @@
+package com.example.typefacemaster;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public abstract class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+
+    protected void onTerminate() {
+    }
+
+    protected void onCreate() {
+    }
+}
